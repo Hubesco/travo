09d973a33110c8ef9ca58aeffa9c44425aac7c5a
Paolo Escobar <paolo.escobar@fluidly.com>
2020-06-02 14:29:32 +0100

    Moves folders

--- src/App/App.spec.js
+++ src/App/App.spec.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import App from "./App";
+import App from "./index";
 
 describe("App", () => {
   it("renders app without crashing", () => {

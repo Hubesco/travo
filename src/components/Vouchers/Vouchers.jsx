f382e4011fb97b633ee8f1a8f1b3d57316de88b2
Paolo Escobar <paolo.escobar@fluidly.com>
2020-07-05 20:36:01 +0100

    Refactors

---
+++ src/components/Vouchers/Vouchers.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+
+import { format, parse } from "../../utils/date";
+import useStyles from "./styles";
+
+function Vouchers({ vouchers, setVouchers, removeVoucher }) {
+  const classes = useStyles();
+
+  return (
+    <TableContainer className={classes.container}>
+      <Table stickyHeader>
+        <TableHead>
+          <TableRow>
+            <TableCell>Company</TableCell>
+            <TableCell>Expiry Date</TableCell>
+            <TableCell>Code</TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {Object.keys(vouchers).length === 0 && (
+            <TableRow>
+              <TableCell colSpan="3">No vouchers :'(</TableCell>
+            </TableRow>
+          )}
+          {Object.values(vouchers).map((v, index) => (
+            <TableRow key={index}>
+              <TableCell>{v.company}</TableCell>
+              <TableCell>{format(parse(v.expiryDate))}</TableCell>
+              <TableCell>{v.code}</TableCell>
+              <TableCell>
+                <button onClick={() => removeVoucher(v.id)}>Remove</button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default Vouchers;
---
+++ src/components/Vouchers/container.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+
+import storage, { STORAGE_KEYS } from "../../infrastructure/storage";
+import Vouchers from "./Vouchers";
+
+function VouchersContainer() {
+  const [vouchers, setVouchers] = useState({});
+
+  useEffect(() => {
+    const fetchVouchers = async () => {
+      const item = await storage.get("vouchers");
+      setVouchers(item.vouchers || {});
+    };
+    fetchVouchers();
+  }, []);
+
+  async function removeVoucher(voucherId) {
+    const item = await storage.get(STORAGE_KEYS.VOUCHERS);
+    const newVouchers = item.vouchers;
+    delete newVouchers[voucherId];
+    await storage.set({ vouchers: newVouchers });
+    setVouchers(newVouchers);
+  }
+
+  return (
+    <Vouchers
+      vouchers={vouchers}
+      setVouchers={setVouchers}
+      removeVoucher={removeVoucher}
+    />
+  );
+}
+
+export default VouchersContainer;
